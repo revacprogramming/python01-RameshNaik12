eec1efa55230a13cf7ac593101926b78566fd587
RameshNaik12 <96806289+RameshNaik12@users.noreply.github.com>
2022-06-08 09:35:17 +0000

    completed

--- ActivitySet01/problem03.py
+++ ActivitySet01/problem03.py
@@ -1,3 +1,19 @@
 # Variables, Expressions & Statements
-
-hrs = float(input("Enter hours? "))
+#fruit='banana'
+#count = 0
+#letter=fruit[3]
+#print(letter)
+#index=0
+#while index < len(fruit):
+#for letter in 'fruit':
+    #if letter == 'a':
+       #count = count+1
+    #print(letter)
+#index=index+1
+#print(x)
+#s = 'Monty Python'
+#print(s[-1 :])
+#a='hello'
+#b=a+'there'
+#c=a+'\t'+'there'
+#print(c)
--- ActivitySet01/problem15.py
+++ ActivitySet01/problem15.py
@@ -1,2 +1,11 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if a>b and b>c:
+  print(f"{a} is the largest number")
+elif b>=a and b>=c:
+  print(f"{b} is largest number")
+else:
+  print(f"{c} is largest number")
